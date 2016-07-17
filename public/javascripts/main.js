1252b7096172d4f5316fb32bca42972249255b56
Mako <mnfeconicu41@gmail.com>
2016-07-17 13:02:16 +0900

    support for input from Leap motion

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -39,6 +39,8 @@ fieldGenerator.generateDoor();
 // set OrbitControls
 //new game.OrbitControls().setOn(gameScene);
 
+// --- input from Keyboard ---
+
 document.addEventListener("keydown" , function (e) {
     var keyCode = e.keyCode;
     var method;
@@ -60,6 +62,50 @@ document.addEventListener("keydown" , function (e) {
         fieldGenerator.generateDoor();
     }
 });
+
+// --- input from LeapMotion ---
+
+function isLeftHandMoveLeft(leftHand) {
+    if (!leftHand || !leftHand.valid) return false;
+    if (leftHand.type !== "left") return false;
+    return (leftHand.palmPosition[0] < -100 && leftHand.palmVelocity[0] < -200);
+}
+function isRightHandMoveRight(rightHand) {
+    if (!rightHand || !rightHand.valid) return false;
+    if (rightHand.type !== "right") return false;
+    return (rightHand.palmPosition[0] > 100 && rightHand.palmVelocity[0] > 200);
+}
+
+var controller = new Leap.Controller({enableGestures: true})
+    .use('screenPosition')
+    .connect()
+    .on('frame', function(frame) {
+        let hand      = frame.hands[0];
+        let otherHand = frame.hands[1];
+        if (!hand) return;
+        let leftHand  = (hand.type === "left")  ? hand: otherHand;
+        let rightHand = (hand.type === "right") ? hand: otherHand;
+
+        let method = ""
+        if (isLeftHandMoveLeft(leftHand)) {
+            method = "left";
+        } else if (isRightHandMoveRight(rightHand)) {
+            method = "right";
+        }
+        if (isLeftHandMoveLeft(leftHand) && isRightHandMoveRight(rightHand)) {
+            method = "both-side"
+        }
+        if (method) console.log(method);
+
+        // Prevent from opening a door far from here.
+        if (player.position.distanceTo(fieldGenerator.getDoor().position) > 500) return true;
+
+        if (fieldGenerator.openDoor(method)) {
+            player.moveForward();
+            fieldGenerator.generateDoor();
+        }
+    });
+
 
 // init mainProcess
 const mainProcess = new game.Game();
