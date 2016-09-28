6c7c67fa04c8462c932b597640dda28c601bd4d2
Mako <mnfeconicu41@gmail.com>
2016-09-28 14:18:36 +0900

    create ResultScene.js

---
+++ public/javascripts/script/ResultScene.js
@@ -0,0 +1,22 @@
+window.game = window.game || {}
+window.game.ResultScene =
+
+class ResultScene extends game.MonoBehavior {
+  constructor() {
+    super();
+    this.gameScene = {};
+    this.objects = {};
+  }
+
+  start() {
+    //
+  }
+
+  update() {
+    //
+  }
+
+  showResult() {
+    //
+  }
+}
