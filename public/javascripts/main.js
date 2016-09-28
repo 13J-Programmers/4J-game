ea596ecea123871a2d63e9d725fa8162fbd4da66
Mako <mnfeconicu41@gmail.com>
2016-09-28 19:33:19 +0900

    create Start scene

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -33,7 +33,16 @@ const gameScene = new game.GameScene(scene, camera, renderer);
 let player = new game.Player().setOn(gameScene);
 
 // set a start scene
-// let startScene = new game.StartScene().setOn(gameScene);
+let startScene = new game.StartScene().setOn(gameScene);
+startScene.showDoors();
+game.KeyController.enable(detectUserInput);
+game.LeapController.enable(detectUserInput);
+
+// this function is invoked when controllers detect user inputs.
+function detectUserInput(method) {
+  startScene.openDoors(method);
+}
+
 
 // set fieldGenerator
 let fieldGenerator = new game.FieldGenerator().setOn(gameScene);
@@ -52,18 +61,26 @@ mainProcess.set(gameScene);
 mainProcess.start();
 
 // game controller
-game.KeyController.enable(detectUserInput);
-game.LeapController.enable(detectUserInput);
+// emit game-start event when tutorial has done
+document.addEventListener('game-start', function () {
+  console.log("done tutorial!!");
 
-// this function is invoked when controllers detect user inputs.
-function detectUserInput(method) {
-  // argument +method+ -- type String is expected
+  game.KeyController.disable();
+  game.LeapController.disable();
 
-  // Prevent from opening a door far from here.
-  if (player.position.distanceTo(fieldGenerator.getDoor().position) > 500) return;
+  game.KeyController.enable(detectUserInput);
+  game.LeapController.enable(detectUserInput);
 
-  if (fieldGenerator.openDoor(method)) {
-    player.moveForward();
-    fieldGenerator.generateDoor();
+  // this function is invoked when controllers detect user inputs.
+  function detectUserInput(method) {
+    // argument +method+ -- type String is expected
+
+    // Prevent from opening a door far from here.
+    if (player.position.distanceTo(fieldGenerator.getDoor().position) > 500) return;
+
+    if (fieldGenerator.openDoor(method)) {
+      player.moveForward();
+      fieldGenerator.generateDoor();
+    }
   }
-}
+});
--- public/javascripts/script/StartScene.js
+++ public/javascripts/script/StartScene.js
@@ -9,45 +9,121 @@ class StartScene extends game.MonoBehavior {
     super();
     this.gameScene = {};
     this.objects = {};
+
+    this.doors = {};
   }
 
   start() {
     let geometry, material;
 
-    // put start scene
+    // put start scene background
     geometry = new THREE.BoxGeometry(500, 200, 0);
     material = new THREE.MeshBasicMaterial({ color: 0x231f20 });
     this.objects.root = new THREE.Mesh(geometry, material);
-    this.objects.root.position.z += game.settings['dist-between-doors'];
+    this.objects.root.position.z += 99;
     this.gameScene.scene.add(this.objects.root);
-
-    // put title "ruNNing AWay" (~= "coNNeCTing")
-    fontLoader.load('/fonts/helvetiker_regular.typeface.json', function (font) {
-      geometry = new THREE.TextGeometry("ruNNing AWay", {
-        size: 10,
-        height: 0.1,
-        curveSegments: 3,
-        font: font,
-        weight: "normal",
-        style: "normal"
-      });
-      material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-      this.objects.titleText = new THREE.Mesh(geometry, material);
-      this.objects.titleText.position.x -= 50;
-      this.objects.titleText.position.y += 30;
-      this.objects.root.add(this.objects.titleText);
-    }.bind(this));
   }
 
   update() {
     //
   }
 
+  showDoors() {
+    // left
+    this.doors.left = new game.Door({
+      type: 2,
+      position: new THREE.Vector3(-10, 0, 102),
+      scale: 0.05,
+      onlyDoor: true
+    })
+    .setOn(this.gameScene);
+
+    // both-side
+    this.doors.bothSide = new game.Door({
+      type: 9,
+      position: new THREE.Vector3(0, 0, 102),
+      scale: 0.05,
+      onlyDoor: true
+    })
+    .setOn(this.gameScene);
+
+    // right
+    this.doors.right = new game.Door({
+      type: 1,
+      position: new THREE.Vector3(10, 0, 102),
+      scale: 0.05,
+      onlyDoor: true
+    })
+    .setOn(this.gameScene);
+
+    // switch
+    this.doors['switch'] = new game.Door({
+      type: 13,
+      position: new THREE.Vector3(-10, -10, 102),
+      scale: 0.05,
+      onlyDoor: true
+    })
+    .setOn(this.gameScene);
+
+    // up
+    this.doors.up = new game.Door({
+      type: 11,
+      position: new THREE.Vector3(0, -10, 102),
+      scale: 0.05,
+      onlyDoor: true
+    })
+    .setOn(this.gameScene);
+
+    // wheel
+    this.doors.wheel = new game.Door({
+      type: 12,
+      position: new THREE.Vector3(10, -10, 102),
+      scale: 0.05,
+      onlyDoor: true
+    })
+    .setOn(this.gameScene);
+  }
+
+  openDoors(method) {
+    for (var door in this.doors) {
+      if (this.doors.hasOwnProperty(door)) {
+        this.doors[door].openSesame(method);
+      }
+    }
+
+    if (this.didOpenAllDoors()) {
+      // hide tutorial
+      setTimeout(function () {
+        this.hideTitle();
+        this.hideTutorial();
+        document.dispatchEvent(new Event('game-start'));
+      }.bind(this), 800);
+    }
+  }
+
+  didOpenAllDoors() {
+    for (var door in this.doors) {
+      if (this.doors.hasOwnProperty(door)) {
+        if (!this.doors[door].isOpened) return false;
+      }
+    }
+    return true;
+  }
+
   showTitle() {
-    $('titlePane').show();
+    $('#titlePane').show();
   }
 
   hideTitle() {
-    $('titlePane').hide();
+    $('#titlePane').hide();
+  }
+
+  hideTutorial() {
+    this.objects.root.visible = false;
+    for (var door in this.doors) {
+      if (this.doors.hasOwnProperty(door)) {
+        this.doors[door].objects.root.visible = false;
+      }
+    }
   }
 }
