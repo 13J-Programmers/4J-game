a6116c1f4ea1059a4af0936097e1d9a4cc024c79
Mako <mnfeconicu41@gmail.com>
2016-10-22 21:30:45 +0900

    refactoring SE.js

--- public/javascripts/script/SE.js
+++ public/javascripts/script/SE.js
@@ -3,135 +3,96 @@ window.game.SE =
 
 class SE extends game.MonoBehavior {
     constructor() {
-        super();
-        this.gameScene = {};
-        this.objects = {};
+      super();
+      this.gameScene = {};
+      this.objects = {};
 
-        this.soundLoader = new THREE.AudioLoader();
+      this.soundLoader = new THREE.AudioLoader();
     }
 
     start() {
-        // set audio
-        this.audioListener = new THREE.AudioListener();
-        this.gameScene.camera.add(this.audioListener);
-        this.sound = new THREE.Audio(this.audioListener);
-        this.gameScene.scene.add(this.sound);
+      // set audio
+      this.audioListener = new THREE.AudioListener();
+      this.gameScene.camera.add(this.audioListener);
 
-        this.sound2 = new THREE.Audio(this.audioListener);
-        this.gameScene.scene.add(this.sound2);
+      this.sound = new THREE.Audio(this.audioListener);
+      this.gameScene.scene.add(this.sound);
 
-        this.sound3 = new THREE.Audio(this.audioListener);
-        this.gameScene.scene.add(this.sound3);
-        this.loadAudioFootsteps();
+      this.sound2 = new THREE.Audio(this.audioListener);
+      this.gameScene.scene.add(this.sound2);
+
+      this.sound3 = new THREE.Audio(this.audioListener);
+      this.gameScene.scene.add(this.sound3);
+      this.loadAudioFootsteps();
     }
 
     update() {
-        //
+      //
+    }
+
+    loadAudio(args, callback) {
+      var audio  = args.audio;
+      var file   = args.file;
+      var isLoop = args.isLoop || false;
+      var volume = args.volume || 1;
+
+      this.soundLoader.load(
+        file,
+        function (audioBuffer) {
+          audio.buffer = null;
+          audio.setBuffer(audioBuffer);
+          audio.setLoop(isLoop);
+          this.audioListener.setMasterVolume(volume);
+          //audio.play();
+          callback();
+        }.bind(this)
+      );
     }
 
     playAudioOpenDoor() {
-      //this.playAudio('se/open_door.mp3', false, 2);
-      var title = 'se/open_door.mp3';
-      var isLoop = false;
-      var volume = 2;
-      console.log(title);
-
-      this.soundLoader.load(
-        title,
-        function (audioBuffer) {
-          this.sound.buffer = null;
-          this.sound.setBuffer(audioBuffer);
-          this.sound.setLoop(isLoop);
-          this.audioListener.setMasterVolume(volume);
-          this.sound.play();
-        }.bind(this),
-        function (xhr) {
-          //console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        function (xhr) {
-          console.log('An error happened');
-        }
-      );
+      var sound = this.sound;
+      this.loadAudio({
+        audio: sound,
+        file: 'se/open_door.mp3',
+        volume: 2
+      }, function successed() {
+        sound.play();
+      });
     }
 
     playAudioCloseDoor() {
-      //this.playAudio('se/close_door.mp3', false, 2);
-      var title = 'se/close_door.mp3';
-      var isLoop = false;
-      var volume = 2;
-      console.log(title);
-
-      this.soundLoader.load(
-        title,
-        function (audioBuffer) {
-          this.sound2.buffer = null;
-          this.sound2.setBuffer(audioBuffer);
-          this.sound2.setLoop(isLoop);
-          this.audioListener.setMasterVolume(volume);
-          this.sound2.play();
-        }.bind(this),
-        function (xhr) {
-          //console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        function (xhr) {
-          console.log('An error happened');
-        }
-      );
+      var sound = this.sound2;
+      this.loadAudio({
+        audio: sound,
+        file: 'se/close_door.mp3',
+        volume: 2
+      }, function successed() {
+        sound.play();
+      });
     }
 
-    // playAudio(title, isLoop, volume) {
-    //     console.log(title);
-    //     this.soundLoader.load(
-    //         title,
-    //         function (audioBuffer) {
-    //             this.sound.buffer = null;
-    //             this.sound.setBuffer(audioBuffer);
-    //             this.sound.setLoop(isLoop);
-    //             this.audioListener.setMasterVolume(volume);
-    //             this.sound.play();
-    //         }.bind(this),
-    //         function (xhr) {
-    //             //console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    //         },
-    //         function (xhr) {
-    //             console.log('An error happened');
-    //         }
-    //     );
-    // }
-
     loadAudioFootsteps() {
-      var title = 'se/running_on_road.mp3';
-      var isLoop = true;
-      var volume = 2;
-
-      this.soundLoader.load(
-        title,
-        function (audioBuffer) {
-          this.sound3.buffer = null;
-          this.sound3.setBuffer(audioBuffer);
-          this.sound3.setLoop(isLoop);
-          this.audioListener.setMasterVolume(volume);
-
-        }.bind(this),
-        function (xhr) {
-          //console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
-        function (xhr) {
-          console.log('An error happened');
-        }
-      );
+      var sound = this.sound3;
+      this.loadAudio({
+        audio: sound,
+        file: 'se/running_on_road.mp3',
+        isLoop: true,
+        volume: 1
+      }, function successed() {
+        sound.play();
+      });
     }
 
     playAudioFootsteps() {
       if (!this.sound3.isPlaying) {
-        console.log("play footsteps");
+        //console.log("play footsteps");
         this.sound3.play();
       }
     }
 
     pauseAudioFootsteps() {
       if (this.sound3.isPlaying) {
-        console.log("pause footsteps");
+        //console.log("pause footsteps");
         this.sound3.pause();
       }
     }
